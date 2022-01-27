320d2d28c7f6fca876f8cc87b8eb95761b62bfe6
Ultra Zacky <ultrazacky@gmail.com>
2022-01-26 16:32:04 -0800

    add disable for the player buttons
    Co-Authored-by: Michael Shamrell <michaelshamrell2001@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-function Player1(roll, total, points) {
+function Player1(roll, points, total) {
   this.roll = roll;
-  this.total = total;
   this.points = points;
+  this.total = total;
+  
 }
-function Player2(roll, total, points) {
+function Player2(roll, points, total) {
   this.roll = roll;
-  this.total = total;
   this.points = points;
+  this.total = total;
+  
 }
 
 const player1 = new Player1(0,0,0);
@@ -24,14 +26,19 @@ function diceRollP1() {
   player1.roll = diceRoll;
   
   if (diceRoll !== 1) { 
-    player1.points = player1.roll;
+    player1.points += player1.roll;
     document.getElementById("rollP1").src = "../img/dice" + diceRoll + ".png";
 
   } else if (diceRoll === 1) {
     player1.roll = 0;
+    player1.points = 0;
     document.getElementById("rollP1").src = "../img/dice1.png";
+    document.getElementById("p1Roll").disabled = true;
+    document.getElementById("p1Hold").disabled = true;
+    document.getElementById("p2Roll").disabled = false;
+    document.getElementById("p2Hold").disabled = false;
 
-  } else if (player1.roll >= 100 || player1.points >= 100) {
+  } else if (player1.total >= 100) {
     alert("Player 1 Wins!!!");
   }
   console.log(player1);
@@ -44,14 +51,19 @@ function diceRollP2() {
   player2.roll = diceRoll
   
   if (diceRoll !== 1) { 
-    player2.roll = (diceRoll + player2.roll);
+    player2.points += player2.roll;
     document.getElementById("rollP2").src = "../img/dice" + diceRoll + ".png";
 
   } else if (diceRoll === 1) {
     player2.roll = 0;
+    player2.points = 0;
     document.getElementById("rollP2").src = "../img/dice1.png";
+    document.getElementById("p2Roll").disabled = true;
+    document.getElementById("p2Hold").disabled = true;
+    document.getElementById("p1Roll").disabled = false;
+    document.getElementById("p1Hold").disabled = false;
 
-  } else if (player2.roll >= 100 || player2.points >= 100) {
+  } else if (player2.total >= 100) {
     alert("Player 2 Wins!!!");
   }
   console.log(player2);
@@ -64,28 +76,32 @@ function P1Hold() {
   
   if ($("button#p1Hold")) {
     const diceRoll = Math.floor( Math.random() * 6 ) +1;
-    player1.points = (player1.roll + player1.total);
+    player1.total = player1.points + player1.total;
+    player1.points = 0;
+    player1.roll = 0;
     document.getElementById("p1Roll").disabled = true;
     document.getElementById("p1Hold").disabled = true;
     document.getElementById("p2Roll").disabled = false;
     document.getElementById("p2Hold").disabled = false;
-    console.log(player1.total);
     console.log(player1.roll);
     console.log(player1.points);
+    console.log(player1.total);
   }
 }
 
 function P2Hold() {
   if ($("button#p2Hold")) {
     const diceRoll = Math.floor( Math.random() * 6 ) +1;
-    player2.points = (player2.roll + player2.total); 
+    player2.total = player2.points + player2.total;
+    player2.points = 0;
+    player2.roll = 0;
     document.getElementById("p2Roll").disabled = true;
     document.getElementById("p2Hold").disabled = true;
     document.getElementById("p1Roll").disabled = false;
     document.getElementById("p1Hold").disabled = false;
-    console.log(player2.total);
     console.log(player2.roll);
     console.log(player2.points);
+    console.log(player2.total);
   }
 }
 
