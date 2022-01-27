29e7c36c9b52b0ff3f2c60ba1a87f703009d3544
Ultra Zacky <ultrazacky@gmail.com>
2022-01-27 11:30:53 -0800

    add styling to page and complete the biz logic
    Co-Authored-by: Michael Shamrell <michaelshamrell2001@gmail.com>
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,19 @@ function Player1(roll, points, total) {
   this.roll = roll;
   this.points = points;
   this.total = total;
-  
+
 }
+
 function Player2(roll, points, total) {
   this.roll = roll;
   this.points = points;
   this.total = total;
-  
+
 }
 
-const player1 = new Player1(0,0,0);
+const player1 = new Player1(0, 0, 0);
 
-const player2 = new Player2(0,0,0);
+const player2 = new Player2(0, 0, 0);
 
 
 
@@ -22,119 +23,155 @@ const player2 = new Player2(0,0,0);
 
 function diceRollP1() {
 
-  const diceRoll = Math.floor( Math.random() * 6 ) +1;
+  const diceRoll = Math.floor(Math.random() * 6) + 1;
   player1.roll = diceRoll;
-  
-  if (diceRoll !== 1) { 
+
+  if (diceRoll !== 1) {
     player1.points += player1.roll;
-    document.getElementById("rollP1").src = "../img/dice" + diceRoll + ".png";
+    document.getElementById("rollP1").src = "../img/dice" + diceRoll + ".jfif";
+
+    $("#player1Points").html("<span id='player1Points'><strong>" + player1.points + "</strong></span>");
+    $("#player1Roll").html("<span id='player1Roll'><strong>" + player1.roll + "</strong></span>");
+    $("#player1Total").html("<span id='player1Total'><strong>" + player1.total + "</strong></span>");
+      
+    if (player1.total + player1.points >= 9) {
+      $("#player1wins").show();
+      $(".content").hide();
+      player1.points = 0;
+      player1.roll = 0;
+      player1.total = 0;
+      player2.points = 0;
+      player2.roll = 0;
+      player2.total = 0;
+      $("#player1Points").html("<span id='player1Points'><strong>0</strong></span>");
+      $("#player1Roll").html("<span id='player1Roll'><strong>0</strong></span>");
+      $("#player1Total").html("<span id='player1Total'><strong>0</strong></span>");
+      $("#player2Points").html("<span id='player2Points'><strong>0</strong></span>");
+      $("#player2Roll").html("<span id='player2Roll'><strong>0</strong></span>");
+      $("#player2Total").html("<span id='player2Total'><strong>0</strong></span>");
+    }
 
   } else if (diceRoll === 1) {
     player1.roll = 0;
     player1.points = 0;
-    document.getElementById("rollP1").src = "../img/dice1.png";
+    $("#player1Points").html("<span id='player1Points'><strong>" + player1.points + "</strong></span>");
+    document.getElementById("rollP1").src = "../img/dice1.jfif";
     document.getElementById("p1Roll").disabled = true;
     document.getElementById("p1Hold").disabled = true;
     document.getElementById("p2Roll").disabled = false;
     document.getElementById("p2Hold").disabled = false;
-
-  } else if (player1.total >= 100) {
-    alert("Player 1 Wins!!!");
   }
-  console.log(player1);
-
 }
 
 function diceRollP2() {
-  
-  const diceRoll = Math.floor( Math.random() * 6 ) +1;
+
+  const diceRoll = Math.floor(Math.random() * 6) + 1;
   player2.roll = diceRoll
-  
-  if (diceRoll !== 1) { 
+
+  if (diceRoll !== 1) {
     player2.points += player2.roll;
-    document.getElementById("rollP2").src = "../img/dice" + diceRoll + ".png";
+    document.getElementById("rollP2").src = "../img/dice" + diceRoll + ".jfif";
+    $("#player2Points").html("<span id='player2Points'><strong>" + player2.points + "</strong></span>");
+    $("#player2Roll").html("<span id='player2Roll'><strong>" + player2.roll + "</strong></span>");
+    $("#player2Total").html("<span id='player2Total'><strong>" + player2.total + "</strong></span>");
+  
+    if (player2.total + player2.points >= 9) {
+      $("#player2wins").show();
+      $(".content").hide();
+      player1.points = 0;
+      player1.roll = 0;
+      player1.total = 0;
+      player2.points = 0;
+      player2.roll = 0;
+      player2.total = 0;
+      $("#player1Points").html("<span id='player1Points'><strong>0</strong></span>");
+      $("#player1Roll").html("<span id='player1Roll'><strong>0</strong></span>");
+      $("#player1Total").html("<span id='player1Total'><strong>0</strong></span>");
+      $("#player2Points").html("<span id='player2Points'><strong>0</strong></span>");
+      $("#player2Roll").html("<span id='player2Roll'><strong>0</strong></span>");
+      $("#player2Total").html("<span id='player2Total'><strong>0</strong></span>");
+    }
 
   } else if (diceRoll === 1) {
-    player2.roll = 0;
     player2.points = 0;
-    document.getElementById("rollP2").src = "../img/dice1.png";
+    player2.roll = 0;
+    $("#player2Points").html("<span id='player2Points'><strong>" + player2.points + "</strong></span>");
+    document.getElementById("rollP2").src = "../img/dice1.jfif";
     document.getElementById("p2Roll").disabled = true;
     document.getElementById("p2Hold").disabled = true;
     document.getElementById("p1Roll").disabled = false;
     document.getElementById("p1Hold").disabled = false;
-
-  } else if (player2.total >= 100) {
-    alert("Player 2 Wins!!!");
   }
-  console.log(player2);
-
 }
 
 //Holding Dice-----
 
 function P1Hold() {
-  
+
   if ($("button#p1Hold")) {
-    const diceRoll = Math.floor( Math.random() * 6 ) +1;
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
     player1.total = player1.points + player1.total;
     player1.points = 0;
     player1.roll = 0;
+    $("#player1Points").html("<span id='player1Points'><strong>" + player1.points + "</strong></span>");
+    $("#player1Roll").html("<span id='player1Roll'><strong>" + player1.roll + "</strong></span>");
+    $("#player1Total").html("<span id='player1Total'><strong>" + player1.total + "</strong></span>");
     document.getElementById("p1Roll").disabled = true;
     document.getElementById("p1Hold").disabled = true;
     document.getElementById("p2Roll").disabled = false;
     document.getElementById("p2Hold").disabled = false;
-    console.log(player1.roll);
-    console.log(player1.points);
-    console.log(player1.total);
   }
 }
 
 function P2Hold() {
+
   if ($("button#p2Hold")) {
-    const diceRoll = Math.floor( Math.random() * 6 ) +1;
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
     player2.total = player2.points + player2.total;
     player2.points = 0;
     player2.roll = 0;
+    $("#player2Points").html("<span id='player2Points'><strong>" + player2.points + "</strong></span>");
+    $("#player2Roll").html("<span id='player2Roll'><strong>" + player2.roll + "</strong></span>");
+    $("#player2Total").html("<span id='player2Total'><strong>" + player2.total + "</strong></span>");
     document.getElementById("p2Roll").disabled = true;
     document.getElementById("p2Hold").disabled = true;
     document.getElementById("p1Roll").disabled = false;
     document.getElementById("p1Hold").disabled = false;
-    console.log(player2.roll);
-    console.log(player2.points);
-    console.log(player2.total);
   }
 }
 
 
-$(document).ready(function(){
-
- 
+$(document).ready(function() {
 
   const player1Roll = $("button#p1Roll").val();
   const player2Roll = $("button#p2Roll").val();
 
-  $("button#p1Roll").click(function(event){
+  $("button#p1Roll").click(function(event) {
     event.preventDefault();
     diceRollP1($("button#p1Roll"));
-    
   });
 
-    $("button#p2Roll").click(function(event){
-      event.preventDefault();
-      diceRollP2($("button#p2Roll"));
+  $("button#p2Roll").click(function(event) {
+    event.preventDefault();
+    diceRollP2($("button#p2Roll"));
+  });
 
-    });
-
-  $("form#player1-hold").click(function(event){
+  $("button.player1-hold").click(function(event) {
     event.preventDefault();
     P1Hold($("button#p1Hold"));
-    
   });
 
-    $("form#player2-hold").click(function(event){
-      event.preventDefault();
-      P2Hold($("button#p2Hold"));
+  $("button.player2-hold").click(function(event) {
+    event.preventDefault();
+    P2Hold($("button#p2Hold"));
+  });
 
-    });
+  $("img.show-game").click(function(event) {
+    event.preventDefault();
+    $("#player1wins").hide();
+    $("#player2wins").hide();
+    $(".content").toggle().show();
+    
+  });
 
 });
